e83aed9d6974e227c4f732697c0703dc68e5b75e
MichioKironex <94037892+MichioKironex@users.noreply.github.com>
2022-01-18 00:26:31 +0900

    製作途中

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>DIALOGUEについて語るサイト</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>DIALOGUEについて語る会<h1>
+    <p>
+</body>
+</html>
+
